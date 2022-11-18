057d1d2321f0258c7088c9182bd75d44f91f772d
Krnsand <krnsandin@gmail.com>
2022-11-18 13:32:48 +0000

    Add runGame function and checkWinner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,22 +20,50 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked rock!")
             } else {
                 let playerChoice = this.getAttribute("data-choice");
-                alert(`You clicked ${playerChoice}!`)
-                /*playgame(playerChoice);*/
+                runGame(playerChoice);
             }
         })
     }
-})
+
+    runGame("rpsls")
+});
 
 /**
  * Main game "loop", called when the script is first loaded
  * and after the users answer has been processed
  */
-function runGame() {
+function runGame(playerChoice) {
 
-    let computerChoice = Math.floor(Math.random() * 5);
+     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+     playerImage.alt = choices[playerChoice];
+ 
+     let computerChoice = Math.floor(Math.random() * 5);
+ 
+     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+     computerImage.alt = choices[computerChoice];
+ 
+     let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+ 
+     updateScores(result);
 }
 
-function checkWinner() {
+function checkWinner(playerChoice, computerChoice) {
 
-}
+    const whatBeatsWhat = {
+        'scissors': 'paper', 'lizard'
+        'paper': 'rock', 'spock'
+        'rock': 'scissors', 'lizard'
+        'lizard': 'paper', 'spock'
+        'spock': 'scissors', 'rock'
+    };
+        if (playerChoice == computerChoice){
+            return 'draw'
+        }
+    
+        if (whatBeatsWhat[playerChoice] == computerChoice) {
+            return 'win'
+        }
+        else {
+            return 'lose'
+        }
+    }
