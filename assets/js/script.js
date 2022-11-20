1108b15c68997ca97df5120cc7cabb9a434e2437
Krnsand <krnsandin@gmail.com>
2022-11-20 09:19:33 +0000

    Add start image when game is reset, add media queries, try to add scroll effect with no luck

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
  /**
- * Declare constants for DOM elements
- * and possible choices
- */
+  * Declare constants for DOM elements
+  * and possible choices
+  */
  const buttons = document.getElementsByClassName("control");
  const playerScoreElement = document.getElementById("player-score");
  const computerScoreElement = document.getElementById("computer-score");
@@ -14,108 +14,110 @@
  let playerScore = 0;
  let computerScore = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+ document.addEventListener("DOMContentLoaded", function () {
+     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("id") == "reset") {
-            resetGame()
+     for (let button of buttons) {
+         button.addEventListener("click", function () {
+             if (this.getAttribute("id") == "reset") {
+                 resetGame()
 
-            } else {
-                
-                let playerChoice = this.getAttribute("data-choice");
-                runGame(playerChoice);
-           }
-        })
-    }
-});
+             } else {
 
-/**
- * Main game "loop", called when the script is first loaded
- * and after the users answer has been processed
- */
-function runGame(playerChoice) {
-    if (movesLeft == 0) {
-        return 
-    }
+                 let playerChoice = this.getAttribute("data-choice");
+                 runGame(playerChoice);
+             }
+         })
+     }
+ });
+
+ /**
+  * Main game "loop", called when the script is first loaded
+  * and after the users answer has been processed
+  */
+ function runGame(playerChoice) {
+     if (movesLeft == 0) {
+         return
+     }
 
      playerImage.src = `assets/images/${choices[playerChoice]}.png`;
      playerImage.alt = choices[playerChoice];
- 
+
      let computerChoice = Math.floor(Math.random() * 5);
- 
+
      computerImage.src = `assets/images/${choices[computerChoice]}.png`;
      computerImage.alt = choices[computerChoice];
- 
+
      let result = checkWinner(choices[computerChoice], choices[playerChoice]);
- 
+
      if (result == 'win') {
-        playerScore += 1
+         playerScore += 1
 
      } else if (result == 'lose') {
-        computerScore += 1
+         computerScore += 1
 
      }
 
-    movesLeft -= 1
-    updateHTML()
+     movesLeft -= 1
+     updateHTML()
 
-    if (movesLeft == 0) {
-        
-        if (playerScore > computerScore) {
-            messages.innerText = "You Win!!"
+     if (movesLeft == 0) {
 
-        } else if (computerScore > playerScore) {
-            messages.innerText = "Awwww... you suck..."
-            
-        } else {
-            messages.innerText = "Draw"
-        }
-    }
-}
+         if (playerScore > computerScore) {
+             messages.innerText = "You Win!!"
 
-/**
- * Updates the HTML without having to call it in multiple places
- */
-function updateHTML() {
-    movesLeftElement.innerText = movesLeft
-    playerScoreElement.innerText = playerScore
-    computerScoreElement.innerText = computerScore
-}
+         } else if (computerScore > playerScore) {
+             messages.innerText = "Computer wins..."
 
-/**
- * Main game code, checks what beats what
- */
-function checkWinner(playerChoice, computerChoice) {
+         } else {
+             messages.innerText = "Draw"
+         }
+     }
+ }
 
-    let whatBeatsWhat = {
-        'scissors': ['paper', 'lizard'],
-        'paper': ['rock', 'spock'],
-        'rock': ['scissors', 'lizard'],
-        'lizard': ['paper', 'spock'],
-        'spock': ['scissors', 'rock']
-    }
-        if (playerChoice == computerChoice){
-            return 'draw'
-        }
-    
-        if (whatBeatsWhat[playerChoice].includes(computerChoice)) {
-            return 'win'
-        }
-        else {
-            return 'lose'
-        }
-    };
+ /**
+  * Updates the HTML without having to call it in multiple places
+  */
+ function updateHTML() {
+     movesLeftElement.innerText = movesLeft
+     playerScoreElement.innerText = playerScore
+     computerScoreElement.innerText = computerScore
+ }
 
-/**
- * Resets game with the press of the reset button
- */
-function resetGame() {
-    movesLeft = 10
-    playerScore = 0
-    computerScore = 0
-    updateHTML()
+ /**
+  * Main game code, checks what beats what
+  */
+ function checkWinner(playerChoice, computerChoice) {
 
-    messages.innerText = ""
-}
+     let whatBeatsWhat = {
+         'scissors': ['paper', 'lizard'],
+         'paper': ['rock', 'spock'],
+         'rock': ['scissors', 'lizard'],
+         'lizard': ['paper', 'spock'],
+         'spock': ['scissors', 'rock']
+     }
+     if (playerChoice == computerChoice) {
+         return 'draw'
+     }
+
+     if (whatBeatsWhat[playerChoice].includes(computerChoice)) {
+         return 'win'
+     } else {
+         return 'lose'
+     }
+ };
+
+ /**
+  * Resets game with the press of the reset button
+  */
+ function resetGame() {
+     movesLeft = 10
+     playerScore = 0
+     computerScore = 0
+     updateHTML()
+
+     playerImage.src = `assets/images/rpsls-left.png`;
+     computerImage.src = `assets/images/rpsls-right.png`;
+
+     messages.innerText = ""
+ }
